976294900e81a73d73251f2fdfb6121bc1a49b83
Luke Powers <luke@openx.com>
2018-03-28 10:05:36 -0700

    changed order of rest/work, makes more sense to start with work

--- scripts/pomodoro.sh
+++ scripts/pomodoro.sh
@@ -14,7 +14,7 @@ if [ -z ${TOO_LATE+x} ]; then
 fi
 
 while [ `date +%k%M` -gt $TOO_EARLY -a `date +%k%M` -lt $TOO_LATE ] ; do
-    zenity --text "Take a rest" --info > /dev/null 2>&1 && sleep 300
-    zenity --text "Back to work" --info > /dev/null 2>&1 && sleep 1500
+    zenity --text "Work" --info > /dev/null 2>&1 && sleep 1500
+    zenity --text "Rest" --info > /dev/null 2>&1 && sleep 300
 done
 zenity --text "Outside of workday, done." --warning > /dev/null 2>&1
