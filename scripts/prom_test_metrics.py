01711b338a4793e50a2ebd872ccc425795d2b247
luke-powers <luke-powers@users.noreply.github.com>
2018-06-19 16:10:42 -0700

    Create prom_test_metrics.py

---
+++ scripts/prom_test_metrics.py
@@ -0,0 +1,22 @@
+import math
+from prometheus_client import start_http_server, Gauge
+import random
+import time
+
+
+METRIC = Gauge('test_metric', 'Test Metrics for Testing', labelnames=['foo', 'bar'])
+
+
+def emit_metric(num):
+    value_1 = int((math.sin(0.025*num) * 500) + 500)
+    METRIC.labels(foo='ok', bar='strange').set(value_1)
+    value_2 = int((math.cos(0.025*num) * 250) + 250)
+    METRIC.labels(foo='meh', bar='bah').set(value_2)
+
+if __name__ == '__main__':
+    start_http_server(8000)
+    ctr = 0
+    while True:
+        emit_metric(ctr)
+        ctr+=1
+        time.sleep(10)
